e6a4359528af9d7b4d9cfc321a65ec8c9c7ce4ad
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-26 02:15:38 +0300

    Add level path

---
+++ Assets/Scripts/LevelPath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Runner {
+
+public class LevelPath : MonoBehaviour {
+    public Vector3[] _points;
+    public int debugDrawPointIndex = 0;
+
+    void Start() {
+        _RetrievePathPoints();
+    }
+
+    // void Update() {
+    //     _DebugDraw();
+    // }
+
+    private void _RetrievePathPoints() {
+        var mesh = GetComponent<MeshFilter>().mesh;
+
+        _points = mesh.vertices
+            .Select(transform.TransformPoint)
+            .ToArray();
+    }
+
+    private void _DebugDraw() {
+        Vector3 x = _points[debugDrawPointIndex];
+        Debug.DrawLine(x, x + Vector3.up, Color.red);
+    }
+}
+
+}
