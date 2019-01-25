054f1ddf6fbe1e64928b5f5861853142e6859087
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-26 01:03:19 +0300

    Add moving player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner {
+
+public class Player : MonoBehaviour {
+    public float speed = 10;
+
+    private Rigidbody _rigidbody;
+
+    void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+        Debug.Assert(_rigidbody != null);
+    }
+
+    void FixedUpdate() {
+        _Move();
+    }
+
+    private void _Move() {
+        var velocity = _rigidbody.velocity;
+        velocity.x = Input.GetAxis("Vertical") * speed;
+        velocity.z = -Input.GetAxis("Horizontal") * speed;
+        _rigidbody.velocity = velocity;
+    }
+}
+
+}
---
+++ Assets/Scripts/_Template.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner {
+
+public class _Template : MonoBehaviour {
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
+
+}
