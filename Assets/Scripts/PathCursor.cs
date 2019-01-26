b0a44e1359fbb81374bf7f62919339ee6d88c571
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-26 04:44:16 +0300

    Refactor: allow to modify PathCursor from outside

--- Assets/Scripts/PathCursor.cs
+++ Assets/Scripts/PathCursor.cs
@@ -6,16 +6,33 @@ namespace Runner {
 
 // Moves along the path.
 public class PathCursor : MonoBehaviour {
-    private LevelPath _path;
-
     // Index of the current point
-    private int currentIndex;
+    public int currentIndex {
+        get => _currentIndex;
+        set { _currentIndex = value; _isDirty = true; }
+    }
 
     // Offset from the current point towards the next one
-    private float offset;
+    public float offset {
+        get => _offset;
+        set { _offset = value; _isDirty = true; }
+    }
+
+    private LevelPath _path;
+    private int _currentIndex;
+    private float _offset;
+    private bool _isDirty = true;
 
     void Start() {
         _path = LevelPath.instance;
+    }
+
+    void Update() {
+        if (_isDirty) {
+            _isDirty = false;
+
+            _UpdateTransform();
+        }
     }
 
     public void Step(float distance) {
@@ -26,7 +43,6 @@ public class PathCursor : MonoBehaviour {
                 return;
             }
 
-            Vector3 currentPoint = _path.positions[currentIndex];
             Vector3 nextPoint = _path.positions[currentIndex + 1];
             float distanceToNext =
                 Vector3.Distance(transform.position, nextPoint);
@@ -39,15 +55,22 @@ public class PathCursor : MonoBehaviour {
                 distanceLeft = 0;
             }
 
-            Vector3 direction = (nextPoint - currentPoint).normalized;
-            transform.position = currentPoint + direction * offset;
-
-            Vector3 directionInterpolated = _GetInterpolatedDirection();
-            transform.LookAt(
-                worldPosition: transform.position + directionInterpolated,
-                Vector3.up
-            );
+            _isDirty = true;
         }
+    }
+
+    private void _UpdateTransform() {
+        Vector3 currentPoint = _path.positions[currentIndex];
+        Vector3 nextPoint = _path.positions[currentIndex + 1];
+
+        Vector3 direction = (nextPoint - currentPoint).normalized;
+        transform.position = currentPoint + direction * offset;
+
+        Vector3 directionInterpolated = _GetInterpolatedDirection();
+        transform.LookAt(
+            worldPosition: transform.position + directionInterpolated,
+            Vector3.up
+        );
     }
 
     private Vector3 _GetDirection(int index) =>
