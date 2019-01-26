1b6a01bed371ac4c9291f14060f44feff51a07b4
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-26 03:56:43 +0300

    Calculate orientation of path cursor

--- Assets/Scripts/PathCursor.cs
+++ Assets/Scripts/PathCursor.cs
@@ -39,9 +39,37 @@ public class PathCursor : MonoBehaviour {
                 _offset += distanceLeft;
                 distanceLeft = 0;
             }
+
             Vector3 direction = (nextPoint - currentPoint).normalized;
             transform.position = currentPoint + direction * _offset;
+
+            Vector3 directionInterpolated = _GetInterpolatedDirection();
+            transform.LookAt(
+                worldPosition: transform.position + directionInterpolated,
+                Vector3.up
+            );
         }
+    }
+
+    private Vector3 _GetDirection(int index) =>
+        index == _path.positions.Length - 1 ?
+            _GetDirection(index - 1) :
+            (_path.positions[index + 1] - _path.positions[index]).normalized;
+
+    private Vector3 _GetInterpolatedDirection() {
+        bool isLastPoint = _currentPoint == _path.positions.Length - 1;
+        if (isLastPoint) {
+            return _GetDirection(_currentPoint);
+        }
+
+        Vector3 currentPoint = _path.positions[_currentPoint];
+        Vector3 nextPoint = _path.positions[_currentPoint + 1];
+        float factor = _offset / Vector3.Distance(currentPoint, nextPoint);
+        return Vector3.Lerp(
+            _GetDirection(_currentPoint),
+            _GetDirection(_currentPoint + 1),
+            factor
+        );
     }
 }
 
