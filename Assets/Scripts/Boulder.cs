694e527883e876a474e6fa583ef5b791438e8c32
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-26 13:14:55 +0300

    Roll the boulder

--- Assets/Scripts/Boulder.cs
+++ Assets/Scripts/Boulder.cs
@@ -6,16 +6,27 @@ namespace Runner {
 
 public class Boulder : MonoBehaviour {
     public float speed = 20;
+    float radius = 5;
 
     private PathCursor _cursor;
+    private Transform _transform;
 
     void Awake() {
         _cursor = GetComponent<PathCursor>();
         Debug.Assert(_cursor != null);
+
+        _transform = transform.Find("Boulder");
+        Debug.Assert(_transform != null);
     }
 
     void Update() {
         _cursor.Step(Time.deltaTime * speed);
+        _Roll();
+    }
+
+    private void _Roll() {
+        float angle = _cursor.totalDistance * 360 / (2 * Mathf.PI * radius);
+        _transform.localEulerAngles = new Vector3(angle, 0, 0);
     }
 }
 
