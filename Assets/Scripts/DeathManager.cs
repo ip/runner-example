3a49ff16a1fe3324b9e9ecf5685953372e2292fe
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-28 02:54:43 +0300

    Add death and restart
    Die on collisions with boulder and when falling into pits

---
+++ Assets/Scripts/DeathManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner {
+
+public class DeathManager : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Kills on collision") {
+            _Die();
+        }
+    }
+
+    private void _Die() {
+        gameObject.SetActive(false);
+
+        RestartDialog.instance.Show(isDead: true);
+    }
+}
+
+}
---
+++ Assets/Scripts/RestartDialog.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Runner {
+
+public class RestartDialog : MonoBehaviour {
+    public static RestartDialog instance;
+
+    private Text _text;
+
+    void Awake() {
+        instance = this;
+
+        _text = transform.Find("Text")
+            .GetComponent<Text>();
+        Debug.Assert(_text != null);
+
+        _BindRestartButton();
+
+        gameObject.SetActive(false);
+    }
+
+    public void Show(bool isDead) {
+        gameObject.SetActive(true);
+
+        _text.text = isDead ? "You're dead!" : "You win!";
+    }
+
+    private void _BindRestartButton() {
+        transform.Find("Restart button").GetComponent<Button>()
+            .onClick.AddListener(_RestartGame);
+    }
+
+    private void _RestartGame() {
+        SceneManager.LoadScene(0);
+    }
+}
+
+}
