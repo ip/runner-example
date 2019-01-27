2752f27a9905107f674bac38f4ff0ca1f59ed8d1
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-28 01:37:08 +0300

    Add crates and player spinning

---
+++ Assets/Scripts/PlayerSpin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner {
+
+public class PlayerSpin : MonoBehaviour {
+    public float spinDuration = 1.5f;
+
+    // Degrees per second
+    public float speed = 360*2f;
+
+    private Transform _transform;
+    private bool _isSpinning = false;
+
+    void Start() {
+        _transform = transform.GetChild(0);
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1") && !_isSpinning) {
+            StartCoroutine(_Spin());
+        }
+    }
+
+    private IEnumerator _Spin() {
+        _isSpinning = true;
+
+        float endTime = Time.time + spinDuration;
+        while (Time.time < endTime) {
+            float deltaAngle = Time.deltaTime * speed;
+            _transform.Rotate(Vector3.up, deltaAngle, Space.Self);
+            yield return new WaitForEndOfFrame();
+        }
+        _transform.localRotation = Quaternion.identity;
+
+        _isSpinning = false;
+    }
+}
+
+}
