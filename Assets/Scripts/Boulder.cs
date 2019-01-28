4ded0b04925ca7cd6da1e462e748e79baa2532ab
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-28 03:26:42 +0300

    Roll boulders

--- Assets/Scripts/Boulder.cs
+++ Assets/Scripts/Boulder.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 namespace Runner {
 
 public class Boulder : MonoBehaviour {
-    public float speed = 20;
-    // public float radius = 5;
+    public float radius = 5;
     public float startAtDistance;
 
     public Animator animator;
 
     private PathCursor _cursor;
-    private Transform _transform;
-
+    private Transform _transformAnimated;
+    private Transform _transformFree;
     private DistanceOnPath _playerDistance;
+
+    // For roll
+    private bool _justFell = true;
+    private float _startDistance;
 
     void Awake() {
         Debug.Assert(animator != null);
@@ -22,8 +25,11 @@ public class Boulder : MonoBehaviour {
         _cursor = GetComponent<PathCursor>();
         Debug.Assert(_cursor != null);
 
-        // _transform = transform.Find("Boulder");
-        // Debug.Assert(_transform != null);
+        _transformAnimated = transform.GetChild(0);
+        Debug.Assert(_transformAnimated != null);
+
+        _transformFree = _transformAnimated.GetChild(0);
+        Debug.Assert(_transformFree != null);
 
         _playerDistance = GameObject.Find("Player")
             .GetComponent<DistanceOnPath>();
@@ -31,18 +37,33 @@ public class Boulder : MonoBehaviour {
     }
 
     void Update() {
-        // TODO
-        // _Roll();
-
         if (_playerDistance.distance > startAtDistance) {
-            animator.enabled = true;
+            _KickOff();
         }
+
+        _Roll();
     }
 
-    // private void _Roll() {
-    //     float angle = _cursor.distance * 360 / (2 * Mathf.PI * radius);
-    //     _transform.localEulerAngles = new Vector3(angle, 0, 0);
-    // }
+    private void _KickOff() => animator.enabled = true;
+
+    // Rolling
+
+    private void _Roll() {
+        if (!_isOnGround) {
+            return;
+        }
+
+        if (_justFell) {
+            _justFell = false;
+            _startDistance = _cursor.distance;
+        }
+
+        float distanceFromStart = _cursor.distance - _startDistance;
+        float angle = distanceFromStart * 360 / (2 * Mathf.PI * radius);
+        _transformFree.localEulerAngles = new Vector3(angle, 0, 0);
+    }
+
+    private bool _isOnGround => _transformAnimated.localPosition.y == radius;
 }
 
 }
