82021fb88e6a56a738d3c793ac81a08e614a4d37
Ivan Perevezentsev <ivan.perevezentsev@gmail.com>
2019-01-28 03:04:40 +0300

    Win dialog

---
+++ Assets/Scripts/WinManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner {
+
+public class WinManager : MonoBehaviour {
+    public float winAtDistance;
+
+    private DistanceOnPath _distanceComponent;
+
+    void Start() {
+        _distanceComponent = GetComponent<DistanceOnPath>();
+        Debug.Assert(_distanceComponent != null);
+
+        Debug.Assert(winAtDistance > 0);
+    }
+
+    void Update() {
+        if (_distanceComponent.distance > winAtDistance) {
+            RestartDialog.instance.Show(isDead: false);
+        }
+    }
+}
+
+}
